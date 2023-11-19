be86062032b2d456a96a7e19361e10ece6185cfb
Daniel Aleksandrovich <rekogz@gmail.com>
2023-11-19 23:36:04 +0500

    Fixed FormTest

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,9 +1,11 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FormTest {
 
@@ -12,22 +14,33 @@ public class FormTest {
         Configuration.browser = "firefox";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen=true;
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+//        executeJavaScript("$('footer').remove()");
     }
 
     @Test
     void FormTest() {
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
         $("#firstName").setValue("Yaroslav");
         $("#lastName").setValue("shurzG");
         $("#userEmail").setValue("shurzG@gmail.com");
-        $(".custom-control-label").click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("88005553535");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__day--030").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("2002");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Eng");
+        $("#userForm").$(byText("English")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.jpg");
         $("#currentAddress").setValue("Some Address");
-        $(".css-1pahdxg-control").click();
-
-//        site govno mamonta, knopki submita net, polovina poley dlya vvoda ne rabotet
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
     }
 }
